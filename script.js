48c03e8a0bbfb9024842d64461f382c57b817e22
egitat <119411456+egitat@users.noreply.github.com>
2023-04-04 12:31:39 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,15 +1,26 @@
-const coolButton = document.
-querySelector('·js-cool');
-const dontButton = document.
-querySelector('·js-dont');
+const input1 = document.querySelector('.js-teksts');
+const plusButton = document.querySelector('.js-plus');
+const minusButton = doument.querySelector('.js-minus');
+const multiplyButton = document.querySelector('.js-multiply');
+const divideButton = document.querySelector('.js.divide');
+const result = document.querySelector('.js-result');
 
-coolButton.addEventLister
-('mouseover', ()=> {
-    window.open('https://www.youtube.com/watch?v=zZaQ9h14Ruw&ab_channel=D%C4%81visL%C4%81p%C4%81ns', '_blank')
+const Add = (skaitlis1, skaitlis2) => {
+    return skaitlis1 + skaitlis2
+}
 
+const Subtract = (skaitlis1, skaitlis2) => {
+    return skaitlis1 - skaitlis2
+}
+
+const Multiply = (skaitlis1, skaitlis2) => {
+    return skaitlis1 * skaitlis2
+}
+
+const Divide = (skaitlis1, skaitlis2) => {
+    return skaitlis1 / skaitlis2
+}
+
+plusButton.addEventListener('click', () => {
+    result.innerHTML = 
 })
-
-dontButton.addEventListener('mouseover', () => {
-    window.open('https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley', '_blank')
-})
-
